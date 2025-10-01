7866aee28e7db8ad350fa3a602bf0f31836dad4d
Gomez, Ezequiel Nicolás <126687091+ezegomez98@users.noreply.github.com>
2025-10-01 18:02:54 -0300

    Add cuit and razonSocial to Empresa SQL operations

--- pasantias/src/main/java/com/seminario/pasantias/persistence/EmpresaMapper.java
+++ pasantias/src/main/java/com/seminario/pasantias/persistence/EmpresaMapper.java
@@ -12,11 +12,11 @@ public interface EmpresaMapper {
 	@Select("SELECT * FROM Empresa WHERE id_empresa = #{id}")
 	Empresa findById(@Param("id") Integer id);
 
-	@Insert("INSERT INTO Empresa(nombre, ciudad, direccion, email_contacto) VALUES(#{nombre}, #{ciudad}, #{direccion}, #{emailContacto})")
+	@Insert("INSERT INTO Empresa(nombre, ciudad, direccion, email_contacto, cuit, razonSocial) VALUES(#{nombre}, #{ciudad}, #{direccion}, #{emailContacto}, #{cuit}, #{razonSocial})")
 	@Options(useGeneratedKeys = true, keyProperty = "idEmpresa")
 	void insert(Empresa empresa);
 
-	@Update("UPDATE Empresa SET nombre=#{nombre}, ciudad=#{ciudad}, direccion=#{direccion}, email_contacto=#{emailContacto} WHERE id_empresa=#{idEmpresa}")
+	@Update("UPDATE Empresa SET nombre=#{nombre}, ciudad=#{ciudad}, direccion=#{direccion}, email_contacto=#{emailContacto}, cuit=#{cuit}, razonSocial=#{razonSocial} WHERE id_empresa=#{idEmpresa}")
 	void update(Empresa empresa);
 
 	@Delete("DELETE FROM Empresa WHERE id_empresa = #{id}")
